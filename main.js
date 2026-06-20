dc73a06438f6f22b4aa24e4eee30318ecc0327a5
md-danish-lg <dk665226@gmail.com>
2026-06-20 13:13:43 +0500

    refactor: adjust grid size calculation and enhance grid creation logic

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function createGrid(rows){
     let mainContainer = document.querySelector(".main-container");
+    let boxSize = (1200 / rows) - 2; 
 
 
-    boxNumber = 0;
+    let boxNumber = 0;
     for(let i = 0; i < rows; i++){
         let row = document.createElement("div");
 
@@ -13,8 +14,8 @@ function createGrid(rows){
             box.id = "box" + boxNumber;
             box.style.border = "1px solid black";
             box.style.backgroundColor = "white";
-            box.style.width = "5vw";
-            box.style.height = "12vh";
+            box.style.width = boxSize + "px";
+            box.style.height = boxSize + "px";
 
             box.addEventListener("mouseenter", () => {
 
@@ -45,8 +46,31 @@ function getRandomColor(){
     return Math.floor(Math.random() * 256);
 
 }
+
+
+function removeGrid(){
+    let mainContainer = document.querySelector(".main-container");
+    mainContainer.replaceChildren();
+
+}
+
 function startGame(){
-    createGrid(10)   
+    let number = 16;
+    createGrid(number)   
+
+
+    let changeButton = document.querySelector(".change-grid")
+    changeButton.addEventListener("click", ()=>{
+
+        number = prompt("Enter the number of Rows X Columns in the grid (1-100): ");
+        while (!(number > 0 && number <= 100)){
+            number = prompt("Enter the number of Rows X Columns in the grid (1-100): ");
+        }
+        removeGrid();
+        createGrid(number);
+
+
+    })
 }
 
 
