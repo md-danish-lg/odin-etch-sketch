b15bca8fe761f17c101fcbeaddf87bea66481c2f
md-danish-lg <dk665226@gmail.com>
2026-06-20 11:45:36 +0500

    add initial HTML structure, CSS styles, and JavaScript grid creation

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("Hello world")
+function createGrid(rows){
+    mainContainer = document.querySelector(".main-container");
+
+
+    for(let i = 0; i < rows; i++){
+        row = document.createElement("div");
+
+        for (let j = 0; j < rows; j++){
+            box = document.createElement("div");
+            box.classList.add("grid-box")
+            box.style.border = "1px solid black";
+            box.style.width = "5vw";
+            box.style.height = "12vh";
+            row.appendChild(box);
+
+        }
+
+
+        mainContainer.appendChild(row)
+        
+
+    }
+    
+}
+
+
+
+
+createGrid(10)
